9066e6d5d8cf95042013f936822803280d91c790
palaogluserdest <palaogluserdest@gmail.com>
2025-09-22 23:49:39 +0300

    Changed all route for main layout and fixed lucide-ract version bug

--- templates/typescript-shadcn/src/utils/router.js
+++ templates/typescript-shadcn/src/utils/router.js
@@ -2,10 +2,21 @@ import { createBrowserRouter } from "react-router";
 import Dashboard from "../components/admin/Dashboard";
 import AdminLayout from "../Layouts/AdminLayout";
 import AdminNotFound from "../components/admin/AdminNotFound";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../components/main/Home";
+import MainNotFound from "../components/main/MainNotFound";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    Component: MainLayout,
+    children: [
+      { index: true, Component: Home },
+      { path: "*", Component: MainNotFound },
+    ],
+  },
+  {
+    path: "/admin",
     Component: AdminLayout,
     children: [
       { index: true, Component: Dashboard },
